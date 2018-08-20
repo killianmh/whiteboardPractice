39fc1799a9e8fc81586b3ed8f676ee5b1fd671d6
killianmh <killianmh@gmail.com>
2018-08-20 12:21:53 -0700

    linked list cycle solution

--- practice.js
+++ practice.js
@@ -4,9 +4,35 @@
 window.onload = function() {
     $('#title h1').html(name);
 
-    $('#answer').html("here is the number " + num + 
-                        " converted to roman numerals: " 
-                        + intToRom(num));
+    // $('#answer').html("here is the number " + num + 
+    //                     " converted to roman numerals: " 
+    //                     + intToRom(num));
+
+
+    // Create instance of a linkedlist class
+    let L1 = new LinkedList();
+    let L2 = new LinkedList();
+
+    // Create an array 
+
+    let array = [1,4,3,6,4,5,6,7,1,3,4];
+
+    // Populate linked list with data from array
+
+    array.forEach(elem => L1.insertNodeAtTail(elem));
+
+    // L1.insertNodeAtTail(16);
+    // L1.deleteNode(1);
+    // L1.deleteNode(5);
+    // let string = L1.detectCycle();
+
+    // Create a cycle in linked list
+    L1.head.next.next.next.next.next.next = L1.head.next.next.next;
+
+    let string = L1.detectCycle();
+
+    $('#answer').html("Does linked list have a cycle? " + string);
+
 
 }
 
@@ -50,19 +76,95 @@ function intToRom(int) {
 
 name = "Detect cycle in linked list";
 
-let list = {
-    length: 3,
-    head: {
-        value: 3,
-        next: {
-            value: 2,
-            next: {
-                value: 1,
-                next: null 
+// Create class for creating singly linked list (ES6 syntax)
+
+class LinkedList {
+    constructor(){
+        this.head = null;
+    }
+
+    insertNodeAtTail(val){
+        let node = {
+            data: val,
+            next: null
+        }
+
+        if (!this.head){
+            this.head = node;
+        }
+        else {
+            let n1 = this.head;
+            while(n1.next){
+                n1 = n1.next;
+            }
+            n1.next = node;
+        }
+    }
+
+    deleteNode(val){
+        if(!this.head){
+            console.log("linked list is empty")
+            return;
+        }
+
+        if(this.head.data === val){
+            this.head = this.head.next;
+        }
+        else {
+            let n1 = this.head;
+            let n2 = n1.next;
+            while(n2){
+                if(n2.data === val){
+                    n1.next = n2.next;
+                    break;
+                }
+                else {
+                    n1 = n2;
+                }
+                n2 = n2.next;
             }
         }
     }
+
+    printList(){
+        if(!this.head){
+            // console.log("linked list is empty")
+            return ("linked list is empty");
+        }
+        else {
+            let n1 = this.head;
+            let n2 = n1.next;
+            
+            let string = "head: " + n1.data + " next: ";
+            let i = 2;
+            // console.log(string);
+            while(n2){
+                // console.log(n2);
+                string += + n2.data + " next: ";
+                n1 = n2;
+                n2 = n2.next;
+                i++;
+            }
+            // console.log(string);
+            return string;
+        }
+    }
+
+    detectCycle (){
+       let n1 = this.head;
+       let n2 = this.head;
+
+       while (n2.next){
+           n1 = n1.next;
+           n2 = n2.next.next;
+
+           if(n1 == n2){
+               return true;
+           }
+       }
+
+       return false;
+    }
 }
 
-// for (var key in list.head)
 
